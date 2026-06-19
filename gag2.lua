345741bfa32c7dca0ca0042926110e10061f60db
SyxhubOnTop <syxhubontop@gmail.com>
2026-06-19 23:55:57 +0900

    Add notification for script maintenance

---
+++ gag2.lua
@@ -0,0 +1,7 @@
+pcall(function()
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "SyxHub",
+        Text = "Script is in maintenance.",
+        Duration = 3
+    })
+end)
